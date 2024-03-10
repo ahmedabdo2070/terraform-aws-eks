f9e91c696a62523904c8d41c0584a60daf20df81
Ahmed Abdo <ahmedabdo2070@gmail.com>
2024-03-10 17:46:18 +0400

    changed region to us-east-1

--- examples/eks_managed_node_group/main.tf
+++ examples/eks_managed_node_group/main.tf
@@ -8,7 +8,7 @@ data "aws_availability_zones" "available" {}
 locals {
   name            = "ex-${replace(basename(path.cwd), "_", "-")}"
   cluster_version = "1.29"
-  region          = "eu-west-1"
+  region          = "us-east-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
